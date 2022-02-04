b7ae3d2a9c671dc9a5106b4de9ee3c4817037ff2
Edoardo Remondini <edoardo.remondini@reti.it>
2022-02-04 22:34:41 +0100

    aggiunte interfaccie Iuserauthservice e Iuserservice per definire firme metodi autenticazione e classe utente

---
+++ testidentityandjwt.DAL/IServices/IUserservice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using testidentityandjwt.DAL.Entities;
+
+namespace testidentityandjwt.DAL.IServices
+{
+    public interface IUserservice
+    {
+        Task<List<MyUser>> Getalluser();
+        Task<MyUser?> Getsingle(string id);
+
+    }
+}
