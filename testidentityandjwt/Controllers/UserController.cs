001f59b30799f41355399373685b9c27fb39a117
Edoardo Remondini <edoardo.remondini@reti.it>
2022-02-04 22:35:39 +0100

    aggiunti metodi get all e get single in usercontroller

---
+++ testidentityandjwt/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testidentityandjwt.DAL.Entities;
+using testidentityandjwt.DAL.IServices;
+using testidentityandjwt.DAL.Services;
+
+namespace testidentityandjwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserservice _userservice;
+
+        public UserController(IUserservice userservice)
+        {
+            _userservice = userservice;
+        }
+
+        [HttpGet,Route("Getallusers")]
+        public async Task<ActionResult> Getallusers()
+        {
+           var alluser=await _userservice.Getalluser();
+            if(!alluser.Any())
+                return NoContent();
+            return Ok(alluser);
+        }
+
+        [HttpGet,Route("Getsingle/{id}")]
+        public async Task<ActionResult> Getsingle(string id)
+        {
+            MyUser? single = await _userservice.Getsingle(id);
+            if (single is null)
+                return NotFound(single);
+            return Ok(single);
+
+
+        }
+    }
+}
