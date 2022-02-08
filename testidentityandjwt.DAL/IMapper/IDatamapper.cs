af880d186e51418f2750aec9125dd71e9f5084e5
Edoardo Remondini <edoardo.remondini@reti.it>
2022-02-08 18:31:10 +0100

    aggiunta interfaccia datamapper e mapper

---
+++ testidentityandjwt.DAL/IMapper/IDatamapper.cs
@@ -0,0 +1,10 @@
+﻿using testidentityandjwt.DAL.DTO;
+using testidentityandjwt.DAL.Entities;
+
+namespace testidentityandjwt.DAL.Mapper
+{
+    public interface IDatamapper
+    {
+        UserDTO mapmyusertodto(MyUser tomap);
+    }
+}
