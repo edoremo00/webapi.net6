bb787b8d521b3942e7c9c697d436a6f6e7405693
Edoardo Remondini <edoardo.remondini@reti.it>
2022-02-08 18:28:19 +0100

    corretti tipi di ritorno metodi da myuser a userdto. iniettato dipendenza datamapper e creato metodo generico per ottenere userdto o myuser a seconda di un parametro opzionale  passato al metodo

--- testidentityandjwt.DAL/Services/Userservice.cs
+++ testidentityandjwt.DAL/Services/Userservice.cs
@@ -5,52 +5,93 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using testidentityandjwt.DAL.Context;
+using testidentityandjwt.DAL.DTO;
 using testidentityandjwt.DAL.Entities;
 using testidentityandjwt.DAL.IServices;
+using testidentityandjwt.DAL.Mapper;
 using testidentityandjwt.DAL.Repository;
 
 namespace testidentityandjwt.DAL.Services
 {
-    public class Userservice : EFRepository,IUserservice
+    public class Userservice : EFRepository, IUserservice
     {
         //private readonly jwtandidentitycontext _context; NON MI SERVE CHIAMA COSTRUTTORE BASE
-        public Userservice(jwtandidentitycontext context) : base(context)
+        private readonly IDatamapper _mapper;
+
+        public Userservice(jwtandidentitycontext context,IDatamapper mapper) : base(context)
         {
-           // _context = context;
+            // _context = context;
+            _mapper = mapper;
         }
 
-       
+
         public async Task<bool> Delete(string id)
         {
-           MyUser? todelete= await Getsingle(id);
-            if(todelete is not null)
+            MyUser? todelete = await Getsinglemyuser(id);
+            if (todelete is not null && todelete.IsDeleted==false)
             {
-                jwtandidentitycontext.Remove(todelete);
-                
+                jwtandidentitycontext.Update(todelete);
+                todelete.IsDeleted = true;
+
             }
             return await jwtandidentitycontext.SaveChangesAsync() > 0;
-              
+
         }
 
-        public virtual async Task<List<MyUser>> Getall()
+        public virtual async Task<List<UserDTO>> Getall()
         {
-            var allusers = await jwtandidentitycontext.Users.ToListAsync();
-            if(!allusers.Any())
-                return new List<MyUser>();
-            return allusers;
+            var allusers = await jwtandidentitycontext.Users.Where(u=>u.IsDeleted==false).AsNoTracking().ToListAsync();
+            if (!allusers.Any())
+                return new List<UserDTO>();
+            return  allusers.Select( u=>_mapper.mapmyusertodto(u)).ToList();
         }
 
-        public async Task<MyUser?> Getsingle(string id)
+        public async Task<UserDTO?> Getsingle(string id)
         {
-            MyUser? user=await jwtandidentitycontext.Users.FindAsync(id);
-            if(user is null)
+            MyUser? user = await jwtandidentitycontext.Users.FindAsync(id);
+            if (user is null || user.IsDeleted)
                 return null;
-            return user;
+            return _mapper.mapmyusertodto(user);
         }
 
-        public MyUser Update(MyUser entity)
+        private async Task<MyUser?> Getsinglemyuser(string id) //get single che opera direttamente su db senza DTO
         {
-           throw new NotImplementedException();
+            MyUser? tosearch=await  jwtandidentitycontext.Users.FindAsync(id);
+            if (tosearch is not null)
+                return tosearch;
+            return null;
         }
+
+        public async Task<object?> GenericGetsingle(string id,bool mappeduser=false)
+        {
+            MyUser? user = await jwtandidentitycontext.Users.FindAsync(id);
+            if (user is null || user.IsDeleted)
+                return null;
+            if(mappeduser)
+                return _mapper.mapmyusertodto(user);
+            return user;
+
+
+        }
+
+        public async Task<UserDTO?> Update(UserDTO entity)
+        {
+            //MyUser? usertoupdate =   await Getsinglemyuser(entity.Userid);
+            MyUser? usertoupdate = (MyUser?)await GenericGetsingle(entity.Userid);
+            if (usertoupdate is not null)
+            {
+                usertoupdate.UserName = entity.Username;
+                usertoupdate.NormalizedUserName = entity.Username.ToUpper();
+                usertoupdate.birthday = entity.Birthday;
+                jwtandidentitycontext.Update(usertoupdate);
+                if (await jwtandidentitycontext.SaveChangesAsync() > 0)
+                    return _mapper.mapmyusertodto(usertoupdate) ;
+
+            }
+            return null;
+
+        }
+
+
     }
 }
