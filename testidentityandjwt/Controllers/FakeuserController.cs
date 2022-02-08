b4bf70002ecaf2fff57bfb2baa850d20f57c1266
Edoardo Remondini <edoardo.remondini@reti.it>
2022-02-08 18:24:15 +0100

    commentato poichè non più necessario

---
+++ testidentityandjwt/Controllers/FakeuserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using testidentityandjwt.DAL.IServices;
+using testidentityandjwt.DAL.Services;
+
+namespace testidentityandjwt.Controllers
+{
+   /* [Route("api/[controller]")]
+    [ApiController]
+    public class FakeuserController : ControllerBase
+    {
+        private readonly IUserservice _fakeuserservice;
+
+        public FakeuserController(IUserservice fakeuserservice)
+        {
+            _fakeuserservice = fakeuserservice; 
+        }
+
+        [HttpGet,Route("fakeusergetall")]
+        public async Task< ActionResult> fakeusergetall()
+        {
+            return Ok(await _fakeuserservice.Getall());
+        }
+    }*/
+}
