845d3b5e5d135636f0ea110b67bcca5c7e8e2ebe
Edoardo Remondini <edoardo.remondini@reti.it>
2022-02-08 18:22:58 +0100

    corretti tipi restituiti da interfaccia e cambiati da Myuser a UserDTO

--- testidentityandjwt.DAL/IServices/IUserservice.cs
+++ testidentityandjwt.DAL/IServices/IUserservice.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using testidentityandjwt.DAL.DTO;
 using testidentityandjwt.DAL.Entities;
 
 namespace testidentityandjwt.DAL.IServices
 {
     public interface IUserservice
     {
-        Task<List<MyUser>> Getall();
-        Task<MyUser> Getsingle(string id);
+        Task<List<UserDTO>> Getall();
+        Task<UserDTO?> Getsingle(string id);
 
-        MyUser Update(MyUser toupdate);
+        Task<UserDTO?> Update(UserDTO entity);
         Task<bool>Delete(string id);
 
     }
