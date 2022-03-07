4016392471b700a6d7f06027bcb8832a730e6dce
Edoardo Remondini <edoardo.remondini@reti.it>
2022-03-07 23:02:15 +0100

    aggiunta classe todo

---
+++ testidentityandjwt.DAL/Entities/Todo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testidentityandjwt.DAL.Entities
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get;set; }=string.Empty;
+        public string Description { get;set; } = string.Empty;
+        public DateTime CreationDate { get;set; }
+        public DateTime LastModifiedDate { get;set;}
+
+        public bool isDone { get;set; }=false;
+        public bool istodoDeleted { get; set; } = false;
+
+        public MyUser? User { get; set; }
+
+        public string? UserId { get; set; }
+
+    }
+}
