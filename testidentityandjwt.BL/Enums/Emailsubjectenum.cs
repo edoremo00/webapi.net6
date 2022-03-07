85ecfaf34aa014fc5e69c59e72d009f820d43428
Edoardo Remondini <edoardo.remondini@reti.it>
2022-03-07 22:57:58 +0100

    aggiunti enum per service bus e per subject email

---
+++ testidentityandjwt.BL/Enums/Emailsubjectenum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace testidentityandjwt.BL.Enums
+{
+    //[Serializable]
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Emailsubjects
+    {
+        [EnumMember(Value ="Resetpassword")]
+        Resetpassword,
+        [EnumMember(Value = "Welcomeemail")]
+        Welcomeemail,
+        [EnumMember(Value = "Deletedaccount")]
+        Deletedaccount,
+        [EnumMember(Value = "password has been changed")]
+        password_has_been_changed
+    }
+}
---
+++ testidentityandjwt.BL/Enums/Eventlabelsservicebus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace testidentityandjwt.BL.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Eventlabelsservicebus
+    {
+        [EnumMember(Value ="UserregisteredEvent")]
+        UserregisteredEvent
+    }
+}
